dd33b54d49769d3b330d3da75a98be566fd9a215
Amrith Mahadevan <160821963+amrithmahadevan@users.noreply.github.com>
2024-03-13 13:36:18 +0530

    Practise

---
+++ Practice/src/main/java/practising_GitHub_Jenkins/Ex1.java
@@ -0,0 +1,10 @@
+package practising_GitHub_Jenkins;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello. This is just a sample program to practice GitHub and Jenkins");
+	}
+
+}
